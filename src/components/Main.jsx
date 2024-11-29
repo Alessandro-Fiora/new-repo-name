52b16738876c0f08195dfb2bd38c5d89b7e79db9
Alessandro-Fiora <fiora.ale97@gmail.com>
2024-11-29 17:05:00 +0100

    filter list

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,11 +3,22 @@ import Label from "./Label";
 
 import { posts } from "../db/posts";
 
+const renderFilters = () => {
+  let filters = [];
+  posts.forEach((post) => {
+    post.tags.forEach((tag) => {
+      if (!filters.includes(tag)) filters.push(tag);
+    });
+  });
+  return filters.map((filter) => <Label>{filter}</Label>);
+};
+
 export default function Main() {
   return (
     <main>
       <div className="container">
         <h1 className="h3 text-center p-3">Il mio blog</h1>
+        <div className="filters-container text-center">{renderFilters()}</div>
         <div className="row row-cols-3">
           {posts
             .filter((post) => post.published)
